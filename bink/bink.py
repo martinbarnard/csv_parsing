800a5495cd4978462b98dfd48afb37f8237ba07d
Martin Barnard <barnard.martin@gmail.com>
2018-03-16 22:13:49 +0000

    tweaking like a tweaker

--- bink/bink.py
+++ bink/bink.py
@@ -13,23 +13,28 @@ import datetime
 # Our deps
 import click
 
-stars = lambda n : click.echo('*'*n)
+stars = lambda n : click.echo(click.style('*'*n, fg='green'))
+title = lambda n : click.echo(click.style('{}'.format(n), fg='yellow'))
 class Pole():
     '''
-    Our mast data
+    This is our csv parser. It will parse the CSV.
+    Some assumptions are being made about the csv file:
+        based on dataset given above...
+        Fields are:
+            Propery Name | Property Address [1] | Property Address [2] | Property Address [3] | Property Address [4] |
+            Unit Name | Tenant Name | Lease Start Date | Lease End Date | Lease Years | Current Rent
+        Dates are formatted as "%d %b %Y" format
+        We are not normalising names (i.e. Name is not the same as nAme or name)
     '''
     def __init__(self,file):
-        '''
-        '''
         self.csv_data = []
         self.mapped_data = {}
         self.read_data(file)
 
     def read_data(self, file):
         '''
-        Read data & populate it in list. Do other stuff with it later
-        We know that Lease Start Date and Lease End Date are dates
-        We know that Lease Years are int & Current Rent are floats
+        Read our csv & build our dataset 
+        @param file (file handle)
         '''
         try:
             reader = csv.DictReader(file)
@@ -46,16 +51,16 @@ class Pole():
             click.echo(e)
             sys.exit(1)
 
-    def rent_ordered(self, num_items = 5, ascending = True):
+    def rent_ordered(self, num_items = 5, ascending = True, field='Current Rent'):
         '''
-        Return the n-items from the list 
+        print out the n-items from the list - ordering by field
         '''
         rv = []
         rev = not ascending
-        rv = sorted(self.csv_data, key=itemgetter('Current Rent'), reverse=rev)
+        rv = sorted(self.csv_data, key=itemgetter(field), reverse=rev)
         rk = rv[0].keys()
         stars(120)
-        click.echo('getting the rent ({} items)'.format(num_items))
+        title('getting {} items ordered by "{}"'.format(num_items, field))
         stars(120)
         click.echo('\t'.join(p for p in rk))
         for row in rv[:num_items]:
@@ -67,18 +72,22 @@ class Pole():
         Will return the items with 25 years lease (['Lease Years'])
         '''
         rev = not ascending
+        # filter by our years, sort by our rent
         items = sorted([row for row in self.csv_data if int(row['Lease Years']) == years], key=itemgetter('Current Rent'), reverse=rev)
+        # Total our rent
         sum_items = sum([float(p['Current Rent']) for p in items])
+
         stars(120)
-        click.echo(' ----- Total Rent calculated: {}'.format(sum_items))
+        title('Getting Lease Info')
         stars(120)
-        click.echo('getting lease info')
-        stars(120)
+
         rk = items[0].keys()
         click.echo('\t'.join(p for p in rk))
 
         for row in items:
             click.echo('\t'.join(str(row[p]) for p in rk))
+
+        title('Total Rent Calculated: {}'.format(sum_items))
 
     def get_lease_between(self, date1, date2):
         '''
@@ -102,11 +111,14 @@ class Pole():
                 nn.append(row)
 
         stars(120)
-        click.echo('Leases with Start Dates between {} and {}'.format(date1_string, date2_string))
+        title('Leases with Start Dates between {} and {}'.format(date1_string, date2_string))
         stars(120)
 
+        rk = nn[0].keys()
+        click.echo('\t'.join(p for p in rk))
         for row in nn:
             click.echo('\t'.join(str(row[p]) for p in row))
+        stars(120)
 
 
     def get_tenant_mast_count(self):
@@ -120,7 +132,7 @@ class Pole():
             else:
                 rv[row['Tenant Name']] = 1
         stars(120)
-        click.echo('Tentant Mast Count')
+        title('Tentant Mast Count')
         stars(120)
         for k,v in sorted(rv.items()):
             click.echo('{}\t\t{}'.format(k,v))
@@ -129,17 +141,16 @@ class Pole():
 @click.command()
 @click.option('--years', type=click.INT, help='Print the # years lease')
 @click.option('--rent', type=click.INT, help='Print the rent')
-@click.option('--reverse', type=click.BOOL, help='Used to reverse normal output order')
+@click.option('--reverse', type=click.BOOL, help='Used to reverse normal output order', default=False)
 @click.option('--num', type=click.INT, help='Number of items to print')
 @click.argument('file', type=click.File('r'))
 def run(file, years, rent, reverse, num):
     '''
     Parse our csv file and display some info
+    Example: bink --years 25 --rent 12  bink/poles.csv
     '''
-    rev = False
     p = Pole(file)
-    if reverse:
-        rev = True
+    rev = reverse
 
     if rent:
         p.rent_ordered()
